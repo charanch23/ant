ce4dfe0e4bff23db01e4322efe6f16b18fbca2aa
charanch23 <charanchelluboina13@gmail.com>
2025-11-11 20:10:57 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "ai-video-backend-hf",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "axios": "^1.4.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2",
+    "express-rate-limit": "^6.8.0",
+    "helmet": "^7.0.0"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.22"
+  },
+  "engines": {
+    "node": "18.x"
+  }
+}
